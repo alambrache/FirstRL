7523e73860d4a31670d19702e3891db12508f2d6
alambrache <lambrache.adrian@gmail.com>
2018-07-30 22:06:54 +0300

    Add FOV

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -1,6 +1,8 @@
 from map_objects.tile import Tile
 from map_objects.rectangle import Rect
 from random import randint
+import libtcodpy as libtcod
+from entity import Entity
 
 class GameMap:
     def __init__(self, width, height):
@@ -12,6 +14,23 @@ class GameMap:
         tiles = [[Tile(True) for y in range(self.height)] for x in range(self.width)]
 
         return tiles
+
+    def place_entities(self, room, entities, max_monsters_per_room):
+        # Get a random number of monsters
+        number_of_monsters = randint(0, max_monsters_per_room)
+
+        for i in range(number_of_monsters):
+            # Choose a random location in the room
+            x = randint(room.x1 + 1, room.x2 - 1)
+            y = randint(room.y1 + 1, room.y2 - 1)
+
+            if not any([entity for entity in entities if entity.x == x and entity.y == y]):
+                if randint(0, 100) < 80:
+                    monster = Entity(x, y, 'o', libtcod.desaturated_green)
+                else:
+                    monster = Entity(x, y, 'T', libtcod.darker_green)
+
+                entities.append(monster)
 
     def create_room(self, room):
         # go through the tiles in the rectangle and make them passable
